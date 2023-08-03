aca6d31d4c2f0e9c1613063dc22a9c85c9f293c0
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2023-08-03 09:37:30 -0300

    Funções com parâmetros

--- 06-funcoes.php
+++ 06-funcoes.php
@@ -43,8 +43,49 @@
     ?>
 
     <p>Estamos no curso de <?= dadosCurso() ?></p>
-    <p><?=dadosCurso()?> é ministrado no Senac penha.</p>
+    <p><?= dadosCurso() ?> é ministrado no Senac penha.</p>
+    <hr>
 
+    <h2>Função com parâmetros (ou argumentos)</h2>
+    <?php
+    function soma($valor1, $valor2, $valor3)
+    {
+        // VARIÁVEL DE ESCOPO LOCAL
+        $total = $valor1 + $valor2 + $valor3;
+        return $total;
+    }
+
+    ?>
+    <!-- Chamando e exibindo diretamente o retorno -->
+    <p>Resultado 1: <?= soma(10, 20, 5) ?></p>
+    <p>Resultado 2: <?= soma(23, 84, 1000.75) ?></p>
+    <?php
+    // Chamando e guardando o retorno
+    $resultado3 = soma(500, 480, 700);
+    ?>
+
+    <p>Resultado 3: <?= $resultado3 ?></p>
+
+    <?php if ($resultado3 > 100) { ?>
+        <p>Vinicius foi demitido!</p>
+    <?php } ?>
+
+    <?php if (soma(2, 3, 10) >= 10) { ?>
+        <p>Kaue reprovado!!</p>
+    <?php }
+    ?>
+
+        <h3>Função com parâmetros opcionais</h3>
+
+<?php
+// $pessoa é um parâmetro opcional com um valor padrão
+function saudacao($mensagem, $pessoa = "Fulano (a)"){
+    return "Olá, $mensagem $pessoa!";
+}
+?>
+<p><?=saudacao("Bom dia","Melissa")?></p>
+<p><?=saudacao("Boa noite","Tanaka")?></p>
+<p><?=saudacao("Boa tarde",)?></p>
 </body>
 
 </html>
