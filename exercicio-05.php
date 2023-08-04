edbdd7d55b4df4cb4b611ba4cb282b1681a90c26
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2023-08-04 08:24:35 -0300

    Html arrumado

--- exercicio-05.php
+++ exercicio-05.php
@@ -6,15 +6,34 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exercicio 05</title>
     <style>
-        .aprovado{
+        .aprovado {
             color: green;
         }
 
-        .reprovado{
+        .reprovado {
             color: red;
+        }
+
+        h1,
+        h2 {
+            text-align: center;
         }
     </style>
 </head>
+
+<h1>Exercicio Usando function</h1>
+<ul>
+    <li>Crie duas variáveis para representar as notas de um aluno</li>
+    <li>Calcule a média destas notas e verifique a situação do aluno (aprovado ou reprovado)</li>
+</ul>
+
+
+<h2>Desafios</h2>
+<ol>
+    <li>Use duas funções: uma para isolar a lógica de cálculo da média, e outra para isolar a lógica de verificação da situação do aluno.</li>
+    <li> Use recursos do CSS para formatar em vermelho caso reprovado, e azul caso aprovado.</li>
+
+</ol>
 
 <body>
     <?php
@@ -34,8 +53,8 @@
         return "<span class='reprovado'>Você Foi reprovado!</span>";
     };
     ?>
-    <p> <?=$aluno?> Sua média é: <?= VerMedia(5, 6)?>,<?=situacao(VerMedia(5, 6))?></p>
-    <p><?=$aluna?> Sua média é: <?= VerMedia(10, 9)?>,<?=situacao(VerMedia(10,9))?></p>
+    <p> <?= $aluno ?> Sua média é: <?= VerMedia(5, 6) ?>,<?= situacao(VerMedia(5, 6)) ?></p>
+    <p><?= $aluna ?> Sua média é: <?= VerMedia(10, 9) ?>,<?= situacao(VerMedia(10, 9)) ?></p>
 
 </body>
 
