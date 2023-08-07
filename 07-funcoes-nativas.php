5863a47455ee8f550c6607aa2ce95d6207ea39fd
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2023-08-07 08:57:14 -0300

    Usando Filtros

--- 07-funcoes-nativas.php
+++ 07-funcoes-nativas.php
@@ -104,9 +104,35 @@ array com dados únicos. */
     <pre><?= var_dump($produtos) ?></pre>
     <pre><?= var_dump($produtosUnicos) ?></pre>
 
+    <hr>
     <h2>Filtros</h2>
+    <p>Recursos/constantes de análise e limpeza de dados aplicados através das funções <code>filter_var()</code> e <code>filter_input()</code>.</p>
 
-    <h2>Segurança</h2>
+    <h3>Validação</h3>
+    <?php
+    $email = "tiago@gmail.com.br";
+    /* Se o e-mail informado for inválido, ou seja, 
+    se não seguir o padrão geral de endereços de e-mail,
+    a função abaixo retornará "false". */
+    ?>
+    <pre> <?= var_dump(filter_var($email, FILTER_VALIDATE_EMAIL)) ?>
+</pre>
+
+    <h3>Sanitização</h3>
+    <?php
+    $ataque = "<script>
+    document.body.innerHTML = '<h1>Sou ráqui! hahahaha :( </h1>'</script>";
+
+    // Execução sem sanitização (Script é válido)   
+    //echo $ataque;   
+
+    $ataqueSanitizado = filter_var($ataque, FILTER_SANITIZE_SPECIAL_CHARS);
+
+    // Execução com sanitização (Script é anulado)
+    echo $ataqueSanitizado;
+    ?>
+    <hr>
+    <h2>Segurança (criptografia de dados)</h2>
 
 </body>
 
