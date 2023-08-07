b339dd2c0c86e364bc901561985d9117df501cb8
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2023-08-07 10:48:00 -0300

    Require e include

---
+++ textos.php
@@ -0,0 +1,11 @@
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, cum. Reiciendis veniam quas quia officiis fugiat distinctio. Suscipit doloremque tempore quos ut tenetur quam, sunt impedit odit, beatae, inventore magni.</p>
+
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt, temporibus. Quidem, animi mollitia molestias corrupti libero atque fugit asperiores omnis repellendus aperiam est et sequi quisquam id eaque enim porro!
+Obcaecati dolor molestias facere repellat, aliquam dicta consequuntur libero quae architecto. Officiis earum libero totam quisquam aspernatur beatae, sunt delectus? Blanditiis pariatur ut minima! Quia accusamus maxime doloremque nam quis.
+Adipisci, voluptates reiciendis! Possimus distinctio dolor quisquam, doloribus culpa alias vitae quo officiis soluta sed illum explicabo quas quia accusamus maxime? Corrupti ducimus vero, explicabo vitae eaque nam vel. Modi.
+Ab fuga quasi qui saepe laborum aperiam repudiandae autem suscipit beatae quo velit animi, nemo aspernatur est, iste tenetur alias fugit error. Ea soluta, expedita ratione commodi repellendus nam atque.
+Accusamus, velit cupiditate iste nihil vero nisi in quam, porro magnam nam voluptatem? Quia harum rem porro non sunt, mollitia corporis est, vero rerum, id voluptatem. Commodi modi consectetur nulla?
+Expedita velit esse nesciunt maxime nulla, harum officia. Expedita laboriosam, quasi accusamus facere voluptas recusandae esse reprehenderit possimus? Quas libero ducimus dolorum pariatur iure. Ab dolorum dolor ipsa quos ea.
+Quas velit facere distinctio corrupti amet iure doloremque, incidunt eaque nostrum beatae tempore odio culpa esse, harum vitae numquam natus laboriosam fugiat similique illo? Quod molestiae quis nam distinctio vitae.</p>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam quod blanditiis culpa? Dignissimos, dolorem vitae! Quo, vero facere illo numquam, commodi molestiae modi mollitia aliquid odio veritatis culpa iure nostrum!</p>
