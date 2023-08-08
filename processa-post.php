7a0c0f2f038a61b5b5b03bd0c1588568c3d76d14
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2023-08-08 11:11:03 -0300

    Formulário com POST
    Programação para formulário requerindo email e nome para avançar

--- processa-post.php
+++ processa-post.php
@@ -12,22 +12,27 @@
     <hr>
     <p>Receber e processar dados via <b>POST</b></p>
 
+    <?php
+    /* Se os campos nome e e-mail estão vazios */
+    if (empty($_POST["nome"]) || empty($_POST["email"]) ){ ?>
+       <p> Você deve preencher nome e e-mail </p>
+        <p> <a href="10-formulario.html">Voltar</a></p>
+    <?php } else {
+    $nome = $_POST["nome"];
+    $email = $_POST["email"];
+    $mensagem = $_POST["mensagem"];
+    ?>
 
-<?php
-$nome = $_POST["nome"];
-$email = $_POST["email"];
-$mensagem = $_POST["mensagem"];
-?>
-   
     <h2>Dados:</h2>
     <ul>
-        <li>Nome: <?=$nome?></li>
-        <li>E-mail: <?=$email?></li>
-        <li>Mensagem: <?=$mensagem?></li>
+        <li>Nome: <?= $nome ?></li>
+        <li>E-mail: <?= $email ?></li>
+        <li>Mensagem: <?= $mensagem ?></li>
     </ul>
 
-
-
+<?php
+    }
+?>
 </body>
 
 </html>
