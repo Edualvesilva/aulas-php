d354bab77a1cbe7b6fe99a7332de29ffc2cfc045
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2023-08-08 09:00:10 -0300

    exercicío 06 Resolvido

--- 09-site/includes/footer.php
+++ 09-site/includes/footer.php
@@ -1,16 +1,16 @@
 </main>
-<footer>
-    <p>Site criado para o curso <b>Téc. Informática para Internet</b></p>
-
-</footer>
 <?php
 date_default_timezone_set("America/Sao_Paulo");
-$currentdatetime = date("d-m-y");
+$currentdatetime = date("d/m/Y");
 $currentHour = date("H:i");
 ?>
+<footer>
+    <p>Site criado para o curso <b>Téc. Informática para Internet</b></p>
+    <p><?=$currentdatetime?></p>
+    <p><?=$currentHour?></p>
 
-<p><?=$currentdatetime?></p>
-<p><?=$currentHour?></p>
+</footer>
+
 </body>
 
 </html>
--- 09-site/includes/header.php
+++ 09-site/includes/header.php
@@ -4,16 +4,39 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Site PHP</title>
+    <title> <?= getPageTitle(); ?></title>
     <link rel="stylesheet" href="css/styles.css">
 </head>
 
 <body>
+    <?php
+    function getPageTitle()
+    {
+        $page = $_SERVER['PHP_SELF'];
 
+        // $pageTitle = pathinfo($page,PATHINFO_FILENAME);
+        $pageTitle = basename($_SERVER["PHP_SELF"]);
+
+        switch ($pageTitle) {
+            case "index.php":
+                return "Página inicial";
+                break;
+            case "cursos.php":
+                return "Cursos";
+                break;
+            case "contato.php":
+                return "Contato";
+                break;
+            case "duvidas.php":
+                return "Dúvidas";
+        }
+    };
+
+    ?>
     <header>
-        <h1>Site PHP</h1>
+        <h1><?= getPageTitle(); ?></h1>
         <nav>
-            <a href="index.php">Home</a>
+            <a href="index.php">Página Inicial</a>
             <a href="cursos.php">Cursos</a>
             <a href="duvidas.php">Dúvidas</a>
             <a href="contato.php">Contato</a>
